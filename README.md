487fe1d4b495d8e110f4fd2c3c99a35ce2a0c540
zandbergen-it <sam@zandbergen.it>
2022-03-22 15:56:11 +0100

    v1

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# frontend-javascript-loops
+# Opdracht beschrijving
+
+Deze opdracht bestaat uit een aantal kleine opdrachtjes om alles wat je zojuist hebt gelezen over arrays en objecten in de praktijk te brengen. Om de opdracht te maken kun je de opdracht clonen of downloaden naar jouw eigen computer.
+
+Dit project bevat de volgende bestanden:
+
+1. `arrays.js`
+2. `objects.js`
+
+Schrijf onder iedere vraag de JavaScript code die gevraagd wordt en check overal met een `console.log()` of het klopt.
+
+## Script runnen
+Als je de code wil runnen kun je dit doen door het volgende in de terminal in te voeren:
+
+`nodemon arrays.js`
+
+Als je wisselt van bestand moet je nodemon eerst stoppen (ctrl + c voor zowel Windows als Mac) en dan opnieuw starten met de nieuwe bestandsnaam, zoals bijvoorbeeld:
+
+`nodemon objects.js`
---
+++ arrays.js
@@ -0,0 +1,48 @@
+// ==========================================
+// 1. Declareer een variabele en sla daar een array met vier jaartallen (zoals 2021) in op. Log dit in de terminal.
+// ==========================================
+const years = ['2018','2019','2020','2021']
+console.log(years)
+
+// ==========================================
+// 2. Declareer een variabele en sla daar een array met drie van jouw hobbies (zoals muziek luisteren en koekjes eten) in op. Log dit in de terminal.
+// ==========================================
+const hobbies = ['Reizen','Moestuin','Motorrijden']
+console.log(hobbies)
+
+// ==========================================
+// 3a. Declareer een variabele met daarin een array met de waardes 3, 4, 5, 7 en 2
+// 3b. Zorg er nu voor dat je de eerste waarde uit die array in de terminal logt             // geeft 3
+// ==========================================
+const number = [3,4,5,7,2]
+console.log(number[0])
+
+// ==========================================
+// 4a. Declareer een variabele met daarin een array met de waardes groen, geel, rood, paars, blauw en oranje.
+// Nu gaan we de waardes weer aanspreken!
+// 4b. Zorg ervoor dat je de vijfde waarde uit bovenstaande array in de terminal logt       // geeft blauw
+// 4c. Zorg ervoor dat je de tweede waarde uit bovenstaande array in de terminal logt       // geeft geel
+// ==========================================
+const colors = ['groen','geel','rood','paars','blauw','oranje']
+console.log(colors[4])
+console.log(colors[1])
+
+// ==========================================
+// 5a. Declareer een variabele met daarin een array met de waardes 21, 22, 23, 25, 25 en log dit in de terminal
+// 5b. Overschrijf één van de items in de array dusdanig dat de cijferreeks netjes doorloopt (je mag de declaratie op de vorige regel niet aanpassen).
+// 5c. Log de array in de terminal                                                          // geeft [ 21, 22, 23, 24, 25 ]
+// ==========================================
+const numbersTwo = [21,22,23,25,25]
+numbersTwo[3] = 24
+console.log(numbersTwo)
+
+// ==========================================
+// 6a. Declareer een variabele met daarin een array met de waardes bladerdeeg, knoflook, spinazie
+// 6b. Overschrijf daarna de waarde bladerdeeg met lasagne bladen (je mag de declaratie op de vorige regel niet aanpassen).
+// 6c. Log de de array in de terminal                                                       // geeft [ 'lasagne bladen', 'knoflook', 'spinazie' ]
+// 6d. Log de lengte van de array in de terminal                                            // geeft 3
+// ==========================================
+const ingredients = ['bladerdeeg', 'knoflook', 'spinazie']
+ingredients[0] = 'lasagne bladen'
+console.log(ingredients)
+console.log(ingredients.length)
