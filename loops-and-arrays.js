4d372e1698305b5613448b381201ab696c67bc51
zandbergen-it <sam@zandbergen.it>
2022-03-30 01:36:58 +0200

    final

---
+++ loops-and-arrays.js
@@ -0,0 +1,87 @@
+// ==========================================
+// Opdracht 1
+// Schrijf een script dat iedere naam in onderstaande array vervangt door een koosnaampje, door er -"je" achter te plakken.
+// Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 namen zou bevatten!
+// ==========================================
+
+const names = ["Henk", "Piet", "Fred", "Joop"];
+
+// Verwachtte uitkomsten:
+// Vóór het script zie je de originele waardes:
+// console.log(names) geeft: ["Henk", "Piet", "Fred", "Joop"]
+// Na jouw script zie je de aangepaste waardes:
+// console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
+
+console.log(names)
+
+for (let i = 0; names.length != i; i++)
+{
+    names[i] += 'je'
+}
+
+console.log(names)
+
+
+
+// ==========================================
+// Opdracht 2
+// Schrijf een script dat ieder EVEN getal in onderstaande array met 2 vermenigvuldigd, en ieder ONEVEN getal met 3
+// Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 getallen zou bevatten!
+// ==========================================
+
+const numbers = [2, 4, 5, 29, 38];
+console.log(numbers)
+
+for (let i = 0; numbers.length != i; i++)
+{
+    // Even number
+    if ((numbers[i] % 2) == 0)
+    {
+        numbers[i] *= 2
+    }
+    // Odd numbers
+    else
+    {
+        numbers[i] *= 3
+    }
+}
+console.log(numbers)
+
+// Verwachtte uitkomsten:
+// Vóór het script zie je de originele waardes:
+// console.log(numbers) geeft: [2, 4, 5, 29, 38];
+// Na jouw script zie je de aangepaste waardes:
+// console.log(numbers) geeft: [ 4, 8, 10, 87, 76 ];
+
+
+
+
+// ==========================================
+// Opdracht 3
+// Schrijf een script dat voor de maat van ieder vierkant in onderstaande array het volume uitrekent.
+// Vervolgens moet de huidige waarde in de array overschreven worden met: "Het volume van [x] is [y]"
+// Het volume is lengte x breedte x hoogte
+// Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 getallen zou bevatten!
+// ==========================================
+
+const squares = [30, 2, 8, 24, 11];
+
+console.log(squares)
+
+for(let i = 0; squares.length != i; i++)
+{
+    squares[i] = 'Het volume van ' + squares[i] + ' is ' + squares[i] * squares[i] * squares[i]
+}
+
+console.log(squares)
+// Verwachtte uitkomsten:
+// Vóór het script zie je de originele waardes:
+// console.log(squares) geeft: [30, 2, 8, 24, 11];
+// Na jouw script zie je de aangepaste waardes:
+// console.log(squares) geeft: [
+//   'Het volume van 30 is 27000',
+//   'Het volume van 2 is 8',
+//   'Het volume van 8 is 512',
+//   'Het volume van 24 is 13824',
+//   'Het volume van 11 is 1331'
+// ]
